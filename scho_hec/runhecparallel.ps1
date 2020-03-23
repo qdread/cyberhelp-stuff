a0b8d74c134d99aeec02651f19cccb48591fb75a
Quentin Read <quentin.read@gmail.com>
2020-03-23 10:14:11 -0400

    debug some more

--- scho_hec/runhecparallel.ps1
+++ scho_hec/runhecparallel.ps1
@@ -1,7 +1,9 @@
 workflow run_hec {
   $script_names = "testcontrolscript","foo","bar","baz"
   foreach -parallel ($name in $script_names) { 
-    "C:\Program Files (x86)\HEC\HEC-HMS\4.3\hec-hms.cmd" -s "Z:\scripts\$($name).script" # Z: should be mapped to /nfs/scho-data/ 
+    InlineScript {
+	  & "C:\Program Files (x86)\HEC\HEC-HMS\4.3\hec-hms.cmd" -s "Z:\scripts\$($name).script" # Z: should be mapped to /nfs/scho-data/ 
+	}
     "$($name) done" 
   }
 }
