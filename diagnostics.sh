d1f3c3bc99bc7d0b73283e64aaf3b91a98e1a7bb
Quentin Read <qread@sshgw02.research.sesync.org>
2020-04-21 11:00:02 -0400

    update diagnostics with email warning

--- diagnostics.sh
+++ diagnostics.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Get percentage use of research-homedirs
 pct=`df --output=pcent /research-home/qread | tr -dc '0-9'`
 
@@ -7,12 +8,21 @@ nodestatus=`sinfo -p sesync,sesyncshared --Format="nodeaiot" | tail -1`
 # Get the number of nodes in "other", meaning bad, status
 IFS='/' read -r -a nbadnodes <<< "$nodestatus"
 
-# If percentage use is over 95% send myself an email
-if [ $pct -ge "95" ]; then
-	mail -s "Research Homedirs is over 95% full!!! Do something, Q!" qread@sesync.org < /dev/null
+logfile="/research-home/qread/diagnostic_emaillog.txt"
+
+# Check whether emails have already been sent today
+researchmailsent=$(grep -E "research.*`date +"%Y.%m.%d"`" $logfile)
+slurmmailsent=$(grep -E "slurm.*`date +"%Y.%m.%d"`" $logfile)
+
+# If percentage use is over 90% send myself an email if none have been sent today
+if [ $pct -ge "90" ] && [ -z "$researchmailsent" ]; then
+	mail -s "Research Homedirs is over 90% full!!! Do something, Q!" qread@sesync.org < /dev/null
+	echo "research homedirs warning sent on $(date +%Y.%m.%d)" >> $logfile
 fi
 	
-# If over half of the slurm nodes are down or drained send myself an email
-if [ ${nbadnodes[2]} -ge "12" ]; then
-	mail -s "Over half the slurm nodes are in a bad state!!! Do something, Q!" qread@sesync.org < /dev/null
+# If at least 6 of the slurm nodes are down or drained send myself an email
+if [ ${nbadnodes[2]} -ge "6" ] && [ -z "$slurmmailsent" ]; then
+	mail -s "Over 1/4 of the slurm nodes are in a bad state!!! Do something, Q!" qread@sesync.org < /dev/null
+	echo "slurm nodes warning sent on $(date +%Y.%m.%d)" >> $logfile
 fi
+
