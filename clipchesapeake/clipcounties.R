94587137854818761441e463ef14bdfb1f73399f
Quentin Read <quentin.read@gmail.com>
2020-04-24 12:14:18 -0400

    change to sf package

--- clipchesapeake/clipcounties.R
+++ clipchesapeake/clipcounties.R
@@ -1,10 +1,12 @@
 # Load a lookup table of FIPS codes and get only the ones in the Chesapeake Bay watershed.
-library(rgdal)
-counties <- readOGR('/nfs/urbanwoodlands-data/Datasets/Boundaries', layer = 'MergedCounties')
-fips <- read.csv('/nfs/qread-data/state_and_county_fips_master.csv')
-fips_counties <- subset(fips, fips %in% counties@data$FIPS)
-fips_east <- subset(fips_counties, state %in% c('DE','DC','MD','NJ','NY','PA','VA'))
-counties_east <- subset(counties, FIPS %in% fips_east$fips)
+library(sf)
+library(readxl)
+
+counties <- st_read('/nfs/urbanwoodlands-data/Datasets/Boundaries/MergedCounties.shp') # county shapefile
+county_lookup <- read_xls('/nfs/public-data/Chesapeake_Conservancy_landcover/ChesapeakeBay_Population_1790_2007.xls') # has all counties in watershed
+
+counties_chesapeake <- subset(counties, FIPS %in% county_lookup$FIPS) # subset shapefile by the lookup table
+counties_chesapeake <- st_union(counties_chesapeake) # merge into a single polygon
 
 # Write the new counties object to a geopackage
-writeOGR(counties_east, dsn = '/nfs/public-data/Chesapeake_Conservancy_landcover/counties_chesapeake_watershed.gpkg', layer = 'counties', driver = 'GPKG')
+st_write(counties_chesapeake, dsn = '/nfs/public-data/Chesapeake_Conservancy_landcover/counties_chesapeake_watershed.gpkg', layer = 'counties')
