b472f8a33d4b299367e4b3d63071ac8634bb0717
Quentin Read <qread@sshgw02.research.sesync.org>
2020-04-24 09:01:35 -0400

    add opts to clipchesapeake

--- clipchesapeake.sh
+++ clipchesapeake.sh
@@ -1,4 +1,6 @@
-gdalwarp -crop_to_cutline -overwrite -dstnodata NULL \
+#!/bin/bash
+
+gdalwarp -multi -co NUM_THREADS=8 -wo NUM_THREADS=8 --config GDAL_CACHEMAX 1000 -wm 1000 -crop_to_cutline -overwrite -dstnodata NULL \
   -cutline  /nfs/urbanwoodlands-data/Datasets/Boundaries/MergedCounties.shp \
   /nfs/public-data/Chesapeake_Conservancy_landcover/Baywide_13Class_20132014.tif \
   /nfs/urbanwoodlands-data/Datasets/Chesapeake_Conservancy_landcover/ClipToMergedCounties_13class_20132014.tif
