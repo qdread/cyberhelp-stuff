86e63d50862e8925c1bee87af842501485c3663c
Quentin Read <quentin.read@gmail.com>
2020-04-25 10:11:55 -0400

    update clip code

--- clipchesapeake/clipchesapeake.sh
+++ clipchesapeake/clipchesapeake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gdalwarp -multi -co NUM_THREADS=8 -wo NUM_THREADS=8 --config GDAL_CACHEMAX 1000 -wm 1000 -crop_to_cutline -overwrite -dstnodata NULL \
+gdalwarp -multi -co NUM_THREADS=8 -wo NUM_THREADS=8 --config GDAL_CACHEMAX 1000 -wm 1000 -tr 3 3 -r mode -crop_to_cutline -overwrite -dstnodata NULL \
   -cutline  /nfs/public-data/Chesapeake_Conservancy_landcover/counties_chesapeake_watershed.gpkg \
   /nfs/public-data/Chesapeake_Conservancy_landcover/Baywide_13Class_20132014.tif \
   /nfs/urbanwoodlands-data/Datasets/Chesapeake_Conservancy_landcover/ClipToMergedCounties_13class_20132014.tif
--- clipchesapeake/clipcounties.R
+++ clipchesapeake/clipcounties.R
@@ -8,5 +8,8 @@ county_lookup <- read_xls('/nfs/public-data/Chesapeake_Conservancy_landcover/Che
 counties_chesapeake <- subset(counties, FIPS %in% county_lookup$FIPS) # subset shapefile by the lookup table
 counties_chesapeake <- st_union(counties_chesapeake) # merge into a single polygon
 
+# Projection of the raster to be clipped is Albers. Set the polygon's projection to this also. Hopefully this will speed up the clipping.
+counties_chesapeake <- st_transform(counties_chesapeake, crs = '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs ')
+
 # Write the new counties object to a geopackage
 st_write(counties_chesapeake, dsn = '/nfs/public-data/Chesapeake_Conservancy_landcover/counties_chesapeake_watershed.gpkg', layer = 'counties')
