736226e8ba05decc9b13ec1232a7b39037b36ef8
Quentin Read <quentin.read@gmail.com>
2020-04-26 21:00:01 -0400

    correct typo in gdaltranslate

--- clipchesapeake/clipchesapeake.sh
+++ clipchesapeake/clipchesapeake.sh
@@ -9,9 +9,9 @@ path="/nfs/public-data/Chesapeake_Conservancy_landcover"
 # 3 meter resolution output
 gdalwarp -tr 3 3 -r mode -of vrt -crop_to_cutline -overwrite -dstnodata NULL \
   -cutline  ${path}/counties_chesapeake_watershed.gpkg ${path}/Baywide_13Class_20132014.tif ${path}/ClipToMergedCounties_13class_20132014_3m.vrt
-gdal_translate -co compress=LZW NUM_THREADS=8 --config GDAL_CACHEMAX 512 ${path}/ClipToMergedCounties_13class_20132014_3m.vrt ${path}/ClipToMergedCounties_13class_20132014_3m.tif
+gdal_translate -co compress=LZW -co NUM_THREADS=8 --config GDAL_CACHEMAX 512 ${path}/ClipToMergedCounties_13class_20132014_3m.vrt ${path}/ClipToMergedCounties_13class_20132014_3m.tif
 
 # 1 meter resolution output (keep in native resolution)
 gdalwarp -of vrt -crop_to_cutline -overwrite -dstnodata NULL \
   -cutline  ${path}/counties_chesapeake_watershed.gpkg ${path}/Baywide_13Class_20132014.tif ${path}/ClipToMergedCounties_13class_20132014_1m.vrt
-gdal_translate -co compress=LZW NUM_THREADS=8 --config GDAL_CACHEMAX 512 ${path}/ClipToMergedCounties_13class_20132014_1m.vrt ${path}/ClipToMergedCounties_13class_20132014_1m.tif
+gdal_translate -co compress=LZW -co NUM_THREADS=8 --config GDAL_CACHEMAX 512 ${path}/ClipToMergedCounties_13class_20132014_1m.vrt ${path}/ClipToMergedCounties_13class_20132014_1m.tif
