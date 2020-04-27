55a6f8f53214c2e3df064817a000feec6d010ea5
Quentin Read <quentin.read@gmail.com>
2020-04-26 20:51:13 -0400

    split warp into warp then translate

--- clipchesapeake/clipchesapeake.sh
+++ clipchesapeake/clipchesapeake.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-gdalwarp -multi -co NUM_THREADS=8 -wo NUM_THREADS=8 --config GDAL_CACHEMAX 1000 -wm 1000 -tr 3 3 -r mode -crop_to_cutline -overwrite -dstnodata NULL \
-  -cutline  /nfs/public-data/Chesapeake_Conservancy_landcover/counties_chesapeake_watershed.gpkg \
-  /nfs/public-data/Chesapeake_Conservancy_landcover/Baywide_13Class_20132014.tif \
-  /nfs/urbanwoodlands-data/Datasets/Chesapeake_Conservancy_landcover/ClipToMergedCounties_13class_20132014.tif
+path="/nfs/public-data/Chesapeake_Conservancy_landcover"
+
+# Split into two steps, first gdalwarp to a virtual raster with correct boundaries and resolution, then translate to a tif with compression
+# Following code found at https://gis.stackexchange.com/questions/89444/file-size-inflation-normal-with-gdalwarp
+# Also set some options to speed up performance following answer at https://gis.stackexchange.com/questions/241806/does-gdal-translate-support-multi-thread
+
+# 3 meter resolution output
+gdalwarp -tr 3 3 -r mode -of vrt -crop_to_cutline -overwrite -dstnodata NULL \
+  -cutline  ${path}/counties_chesapeake_watershed.gpkg ${path}/Baywide_13Class_20132014.tif ${path}/ClipToMergedCounties_13class_20132014_3m.vrt
+gdal_translate -co compress=LZW NUM_THREADS=8 --config GDAL_CACHEMAX 512 ${path}/ClipToMergedCounties_13class_20132014_3m.vrt ${path}/ClipToMergedCounties_13class_20132014_3m.tif
+
+# 1 meter resolution output (keep in native resolution)
+gdalwarp -of vrt -crop_to_cutline -overwrite -dstnodata NULL \
+  -cutline  ${path}/counties_chesapeake_watershed.gpkg ${path}/Baywide_13Class_20132014.tif ${path}/ClipToMergedCounties_13class_20132014_1m.vrt
+gdal_translate -co compress=LZW NUM_THREADS=8 --config GDAL_CACHEMAX 512 ${path}/ClipToMergedCounties_13class_20132014_1m.vrt ${path}/ClipToMergedCounties_13class_20132014_1m.tif
